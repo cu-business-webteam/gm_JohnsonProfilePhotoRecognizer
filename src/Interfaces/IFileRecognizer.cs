7cfcedd4cd21826fff0f0570a7de8a491b39a51a
uniblab <tjb284@cornell.edu>
2018-08-07 14:58:39 -0400

    initial seed
    Signed-off-by: uniblab <tjb284@cornell.edu>

---
+++ src/Interfaces/IFileRecognizer.cs
@@ -0,0 +1,11 @@
+﻿namespace Johnson.ProfilePhotoRecognizer {
+
+	/// <include file='.\Doc\XmlDoc.xml' path='/types/type[@name="Johnson.ProfilePhotoRecognizer.IFileRecognizer"]/member[@name=""]/*'/>
+	public interface IFileRecognizer {
+
+		/// <include file='.\Doc\XmlDoc.xml' path='/types/type[@name="Johnson.ProfilePhotoRecognizer.IFileRecognizer"]/member[@name="IsRecognized"]/*'/>
+		System.String IsRecognized( System.String fileSpecification );
+
+	}
+
+}
